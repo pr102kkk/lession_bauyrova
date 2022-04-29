58504a82e27d10d644ec63c94a559f0d7b8a8811
Alex <alexiv.04@mail.ru>
2022-04-29 14:42:12 +0700

    committ

---
+++ app/src/main/java/com/example/lession_bauyrova/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lession_bauyrova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
